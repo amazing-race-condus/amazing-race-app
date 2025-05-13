d20acbadfb884c254b0b383b99463e29e184d2f7
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-05-13 13:43:30 +0300

    Initialize backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Response } from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.get('/ping', (_, res: Response) => {
+  res.send('Pong!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
