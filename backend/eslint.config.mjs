00e2038f346e08d92bbe6794b1537b3ae36d3bb7
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-05-13 13:55:43 +0300

    Added linting to backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+
+  { ignores: ["node_modules", "eslint.config.mjs", "dist"] },
+
+  {
+    rules: {
+      quotes: ["error", "double"],
+      indent: ["error", 2],
+      eqeqeq: ["error"],
+      "no-trailing-spaces": ["error"],
+      "no-duplicate-imports": ["error"],
+      "eol-last": ["error", "always"],
+      "no-var": ["error"],
+      "prefer-const": ["error"],
+    }
+  }
+]);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,14 @@
-import express, { Response } from 'express'
+import express, { Response } from "express"
 
 const app = express()
 const port = 3000
 
-app.get('/', (_, res: Response) => {
-  res.send('Hello World!')
+app.get("/", (_, res: Response) => {
+  res.send("Hello World!")
 })
 
-app.get('/ping', (_, res: Response) => {
-  res.send('Pong!')
+app.get("/ping", (_, res: Response) => {
+  res.send("Pong!")
 })
 
 app.listen(port, () => {
