38313e078244d0e37b23b022ba07571ae60bba1c
acpeltol <apeltola717@gmial.com>
2025-05-14 14:44:23 +0300

    Add pipline thorugh frontend-backend-database

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const url = process.env.EXPO_PUBLIC_BACKEND_URL
 
   const [data, setData] = useState(null);
+
+  const pipeline = async () => {
+    // try {
+    //   const response = await fetch(`${url}/pipeline`);
+    //   const result = await response.json();
+    //   alert(result.message || JSON.stringify(result));
+    // } catch (error) {
+    //   alert('Error fetching pipeline');
+    //   console.error(error);
+    // }
+
+    try {
+      const response = await fetch(`${url}/pipeline`);
+      const result = await response.json(); // parse as JSON
+
+      if (Array.isArray(result)) {
+        const messages = result.map(item => item.message || JSON.stringify(item)).join('\n');
+        alert(messages);
+      } else {
+        alert(result.message || JSON.stringify(result));
+      }
+    } catch (error) {
+      alert('Error fetching pipeline');
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const getPong = async () => {
@@ -23,8 +49,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello EETu!</Text>
       <Text>{data ? `Response: ${data}` : 'Loading...'}</Text>
+      <Button
+        title="Test Pipeline" onPress={pipeline}
+      />
       <StatusBar style="auto" />
     </View>
   );
