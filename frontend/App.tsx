73e3e16ff7687b43f6a9937c3071bfc87febca52
acpeltol <apeltola717@gmial.com>
2025-05-14 13:01:50 +0300

    Add possibility to Front-backend communication

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const url = process.env.EXPO_PUBLIC_BACKEND_URL
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getPong = async () => {
+      try {
+        const response = await fetch(`${url}/ping`);
+        const result = await response.text();
+        setData(result.message || JSON.stringify(result));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getPong();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Text>{data ? `Response: ${data}` : 'Loading...'}</Text>
       <StatusBar style="auto" />
     </View>
   );
