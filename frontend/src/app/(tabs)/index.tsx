e71e8cb1b2d245566b36ded48300c9120b129bc0
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-05-15 14:10:16 +0300

    Add typescript types, change web url variable

--- frontend/src/app/(tabs)/index.tsx
+++ frontend/src/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { styles } from "@/styles/commonStyles"
 
 const url =
   Platform.OS === 'web'
-    ? process.env.EXPO_PUBLIC_BACKEND_URL_WEB
+    ? process.env.EXPO_PUBLIC_WEB_BACKEND_URL
     : process.env.EXPO_PUBLIC_BACKEND_URL
 
 const pipeline = async () => {
