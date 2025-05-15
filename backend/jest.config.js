84dda2a2e3fa14315520321ecc8708a08bb1d298
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-05-15 14:33:15 +0300

    Configure jest to use test database

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,10 @@
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["./tests/jest.setup.ts"],
+}
+
+export default config
 export const preset = "ts-jest";
 export const testEnvironment = "node";
 export const testMatch = ["**/tests/**/*.test.ts"];
