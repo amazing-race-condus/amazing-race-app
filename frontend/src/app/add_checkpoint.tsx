63e1cc68d546cf0f452bbdd0c68657aa386efd49
acpeltol <apeltola717@gmial.com>
2025-05-19 14:32:48 +0300

    Refactor checkpoits to redux

--- frontend/src/app/add_checkpoint.tsx
+++ frontend/src/app/add_checkpoint.tsx
@@ -1,27 +1,33 @@
 import { View, Text, TextInput, Pressable } from "react-native"
+import { useDispatch, Provider } from 'react-redux'
+import type { AppDispatch } from '@/store/store'
+// eslint-disable-next-line no-duplicate-imports
+import store from '@/store/store'
 import { Stack, useRouter } from "expo-router"
 import { styles } from "@/styles/commonStyles"
 import React, { useState } from 'react'
 import { Checkpoint } from '@/types'
-import { createCheckpoint } from '@/services/checkpointService'
+import { addCheckpoitReducer } from '@/reducers/checkpointsSlice'
 
 const AddCheckpoint = () => {
+  const dispatch = useDispatch<AppDispatch>()
   const [name, setName] = useState('')
   const router = useRouter()
 
   const checkpointCreation = async () => {
-    try {
-      const newCheckpoint: Checkpoint = {
-        name: name,
-        id: '0'
-      }
-      await createCheckpoint(newCheckpoint)
-      setName('')
-      router.navigate(`/checkpoints`)
-    } catch (error) {
-      alert('Something went wrong!')
-      console.error(error)
+    // try {
+    const newCheckpoint: Checkpoint = {
+      name: name,
+      id: "0"
     }
+
+    dispatch(addCheckpoitReducer(newCheckpoint))
+    setName('')
+    router.navigate(`/checkpoints`)
+    // } catch (error) {
+    //   alert('Something went wrong!')
+    //   console.error(error)
+    // }
   }
 
   return (
@@ -42,4 +48,10 @@ const AddCheckpoint = () => {
   )
 }
 
-export default AddCheckpoint
+const AddCheckpointProvider = () => (
+  <Provider store={store}>
+    <AddCheckpoint />
+  </Provider>
+)
+
+export default AddCheckpointProvider
--- frontend/src/app/checkpoints.tsx
+++ frontend/src/app/checkpoints.tsx
@@ -1,36 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, FlatList, Pressable} from "react-native"
+import { useDispatch, useSelector, Provider } from 'react-redux'
+import store from '@/store/store'
 import { Stack } from "expo-router"
 import { styles } from "@/styles/commonStyles"
-import { Checkpoint } from '@/types'
-import { getAllCheckpoints, removeCheckpoint } from '@/services/checkpointService'
+import { fetchCheckpoints, removeCheckpointReducer } from '@/reducers/checkpointsSlice'
+// eslint-disable-next-line no-duplicate-imports
+import type { RootState, AppDispatch } from '@/store/store'
 
 const Checkpoints = () => {
-  const [checkpoints, setCheckpoints] = useState<Checkpoint[]>([])
+  const dispatch: AppDispatch = useDispatch<AppDispatch>()
+  const checkpoints = useSelector((state: RootState) => state.checkpoints)
 
   useEffect(() => {
-    const fetchCheckpoints = async () => {
-      const allCheckpoints = await getAllCheckpoints()
-      setCheckpoints(allCheckpoints)
-    }
-    fetchCheckpoints()
+    dispatch(fetchCheckpoints())
   }, [])
-
-  const handleRemoveCheckpoint = (id: string) => {
-    try {
-      removeCheckpoint(id).then(() => {
-        setCheckpoints(checkpoints.filter(checkpoint => checkpoint.id !== id))
-      })
-    } catch (error) {
-      alert('Something went wrong!')
-      console.error(error)
-    }
-  }
 
   const CheckpointItem = ({ name, id }: { name: string, id: string }) => (
     <View style={styles.item}>
       <Text style={styles.checkpointName}>{name}</Text>
-      <Pressable style={styles.button} onPress={() => { handleRemoveCheckpoint(id) }}>
+      <Pressable style={styles.button} onPress={() => { dispatch(removeCheckpointReducer(id)) }}>
         <Text style={styles.buttonText}>Poista</Text>
       </Pressable>
     </View>
@@ -58,4 +47,10 @@ const Checkpoints = () => {
   )
 }
 
-export default Checkpoints
+const CheckpointProvider = () => (
+  <Provider store={store}>
+    <Checkpoints />
+  </Provider>
+)
+
+export default CheckpointProvider
