6785223a772c090be88e2b804a4909024f453064
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-05-22 09:57:18 +0300

    Change backend to serve frontend build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { PrismaClient } from "../prisma/prisma/"
 import cors from "cors"
+import path from "path"
 import checkpointsRouter from "../controllers/checkpoints"
 
 const prisma = new PrismaClient()
@@ -8,14 +9,19 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "../public/dist")))
 const port = 3000
 
-app.use("/checkpoints", checkpointsRouter)
+app.use("/api/checkpoints", checkpointsRouter)
+
+app.all("{*splat}", (req, res) => {
+  if (!req.path.startsWith("/api")) {
+    res.sendFile(path.join(__dirname, "../public/dist", "index.html"))
+  }
+})
 
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
 
 export { app, server, prisma }
