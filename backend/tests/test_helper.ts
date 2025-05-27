bf60abf09d384cfdb0a419f5c06bf2353912d39c
Katri <katri.laamala@outlook.com>
2025-05-27 13:38:16 +0300

    add test helper

---
+++ backend/tests/test_helper.ts
@@ -0,0 +1,79 @@
+import { Type } from "../prisma/prisma/"
+
+export const initialCheckpoints = [
+  {
+    name: "Oodi",
+    type: Type.FINISH,
+  },
+  {
+    name: "Tripla",
+    type: Type.START,
+  },
+  {
+    name: "Kumpulan kampus",
+    type: Type.INTERMEDIATE,
+  }
+]
+
+export const finalCheckpoints = [
+  {
+    name: "Oodi",
+    type: Type.FINISH,
+  },
+  {
+    name: "Tripla",
+    type: Type.START,
+  },
+  {
+    name: "Kumpulan kampus",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Rautatieasema",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Esplanadi",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Tuomiokirkko",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Kluuvi",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Kauppatori",
+    type: Type.INTERMEDIATE,
+  },
+]
+
+export const intermediateCheckpoints = [
+  {
+    name: "Kumpulan kampus",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Rautatieasema",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Esplanadi",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Tuomiokirkko",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Kluuvi",
+    type: Type.INTERMEDIATE,
+  },
+  {
+    name: "Kauppatori",
+    type: Type.INTERMEDIATE,
+  },
+]
+
