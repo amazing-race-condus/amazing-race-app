75d69e947f8b3db9912352c1636dc9cf4221201f
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-06-05 12:22:39 +0300

    Refactor Game and settings components

---
+++ frontend/src/components/settings/GameView.tsx
@@ -0,0 +1,46 @@
+import { setStartReducer, setEndReducer } from "@/reducers/eventSlice"
+import { AppDispatch } from "@/store/store"
+import { styles } from "@/styles/commonStyles"
+import { handleAlert } from "@/utils/handleAlert"
+import { View, Pressable, Text } from "react-native"
+import { useDispatch } from "react-redux"
+
+const EVENTID = 1
+
+const GameView = () => {
+  const dispatch: AppDispatch = useDispatch<AppDispatch>()
+
+  const handleStart = () => {
+    handleAlert({
+      confirmText: "Aloita",
+      title: "Vahvista aloitus",
+      message: "Oletko varma että haluat aloittaa pelin?",
+      onConfirm: () => dispatch(setStartReducer(EVENTID))
+    })
+  }
+
+  const handleEnd = () => {
+    handleAlert({
+      confirmText: "Lopeta",
+      title: "Vahvista lopetus",
+      message: "Oletko varma että haluat lopettaa pelin?",
+      onConfirm: () => dispatch(setEndReducer(EVENTID))
+    })
+  }
+
+  return(
+    <View style={styles.content}>
+      <Text style={styles.header}>Hallinoi Peliä:</Text>
+
+      <Pressable style={styles.bigButton} onPress={() => handleStart()}>
+        <Text style={styles.buttonText}>Aloita peli</Text>
+      </Pressable>
+
+      <Pressable style={styles.bigButton} onPress={() => handleEnd()}>
+        <Text style={styles.buttonText}>Lopeta peli</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default GameView
