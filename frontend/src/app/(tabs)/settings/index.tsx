567707fa46be2089498afeaef9ed4fbf36abb7b4
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-06-16 09:53:48 +0300

    Add limited version of settings view back to normal users

--- frontend/src/app/(tabs)/settings/index.tsx
+++ frontend/src/app/(tabs)/settings/index.tsx
@@ -3,34 +3,43 @@ import { styles } from "@/styles/commonStyles"
 import SettingsItem from "@/components/settings/SettingsItem"
 import { useSelector } from "react-redux"
 import { RootState } from "@/store/store"
+import Logout from "@/components/ui/Logout"
 
 const Settings = () => {
   const event = useSelector((state: RootState) => state.event)
+  const user = useSelector((state: RootState) => state.user)
   const gameFinished = Boolean(event.endTime)
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Asetukset</Text>
-      <SettingsItem
-        text="Hallinnoi rasteja"
-        link="/settings/checkpoints"
-      />
-      <SettingsItem
-        text="Hallinnoi ryhmiä"
-        link="/settings/groups"
-      />
-      <SettingsItem
-        text="Hallinnoi reittejä"
-        link="/settings/routes"
-      />
-      <SettingsItem
-        text="Hallinnoi käyttäjiä"
-        link="/settings/users"
-      />
-      <SettingsItem
-        text="Hallinnoi peliä"
-        link="/settings/game"
-      />
+      { user.admin ?
+        <>
+          <SettingsItem
+            text="Hallinnoi rasteja"
+            link="/settings/checkpoints"
+          />
+          <SettingsItem
+            text="Hallinnoi ryhmiä"
+            link="/settings/groups"
+          />
+          <SettingsItem
+            text="Hallinnoi reittejä"
+            link="/settings/routes"
+          />
+          <SettingsItem
+            text="Hallinnoi peliä"
+            link="/settings/game"
+          />
+          <SettingsItem
+            text="Hallinnoi käyttäjiä"
+            link="/settings/users"
+          />
+        </>
+        :
+        // todo: better styling for this
+        <Logout />
+      }
       <SettingsItem
         text="Hallinnoi tapahtumia"
         link="/settings/events"
