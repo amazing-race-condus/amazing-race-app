cda006868c2da57a7544a7d9b8f2537c0033964e
Katri <katri.laamala@outlook.com>
2025-06-17 08:55:29 +0300

    update nodemailer transport

--- backend/src/utils/emailUtils.ts
+++ backend/src/utils/emailUtils.ts
@@ -4,16 +4,17 @@ import { Email } from "@/types"
 export const Mailer = async (message: Email): Promise<void> => {
 
   const transport = nodemailer.createTransport({
-    service: "gmail",
+    pool: true,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
-      pass: process.env.EMAILPW
+      pass: process.env.EMAILPW,
     },
-    ...(process.env.NODE_ENV !== "production" && {
-      tls: {
-        rejectUnauthorized: false
-      }
-    })
+    tls: {
+      servername: "smtp.gmail.com",
+    },
   })
   await transport.sendMail(message)
 }
