1f1749ffdbefcd783ef1c52fd5c9c94c32b54b08
eeritvan <eetu.ritvanen@elisanet.fi>
2025-06-19 11:13:21 +0300

    Add tests for <finishedgroupsrankings />

---
+++ frontend/src/components/results/__tests__/FinishedGroupsRankings.test.tsx
@@ -0,0 +1,90 @@
+import { render, screen } from "@testing-library/react-native"
+import FinishedGroupsRankings from "../FinishedGroupsRankings"
+import { group, disqualifiedGroup, dnfGroup, events, checkpoint } from "@/utils/testUtils"
+import { Group } from "@/types"
+
+jest.mock("@/utils/storageUtil", () => ({
+  storageUtil: {
+    setUser: jest.fn(),
+    getUser: jest.fn(),
+    removeUser: jest.fn(),
+  },
+}))
+
+const mockEvent = events[0]
+
+export const finishGroup: Group = {
+  name: "Voittajat",
+  id: 1010353225246231,
+  members: 5,
+  disqualified: false,
+  dnf: false,
+  penalty: [],
+  easy: false,
+  eventId: 1,
+  nextCheckpointId: null,
+  route: [checkpoint],
+  finishTime: new Date("2025-06-25 22:00:00")
+}
+
+export const finishGroup2: Group = {
+  name: "Toiset",
+  id: 1010353225246231,
+  members: 5,
+  disqualified: false,
+  dnf: false,
+  penalty: [],
+  easy: false,
+  eventId: 1,
+  nextCheckpointId: null,
+  route: [checkpoint],
+  finishTime: new Date("2026-06-25 22:00:00")
+}
+
+describe("<FinishedGroupsRankings />", () => {
+  describe("Rendering", () => {
+    test("renders nothing when no finished groups exist", () => {
+      const groups = [group, dnfGroup]
+      
+      render(
+        <FinishedGroupsRankings groups={groups} event={mockEvent} />
+      )
+
+      expect(screen.queryByText("Maaliin tulleet (0)")).toBeTruthy()
+      expect(screen.queryByText("Testers")).toBeNull()
+      expect(screen.queryByText("Kesken")).toBeNull()
+
+    })
+
+    test("renders finished groups when they exist", () => {
+      const groups = [group, dnfGroup, disqualifiedGroup, finishGroup]
+      
+      render(
+        <FinishedGroupsRankings groups={groups} event={mockEvent} />
+      )
+
+      expect(screen.getByText("Maaliin tulleet (1)")).toBeTruthy()
+      expect(screen.getByText("Voittajat")).toBeTruthy()
+      expect(screen.getByText("1.")).toBeTruthy()
+
+      expect(screen.queryByText("Testers")).toBeNull()
+    })
+
+    test("renders multiple disqualified groups correctly", () => {
+      const groups = [group, dnfGroup, finishGroup, finishGroup2]
+      
+      render(
+        <FinishedGroupsRankings groups={groups} event={mockEvent} />
+      )
+
+      expect(screen.getByText("Maaliin tulleet (2)")).toBeTruthy()
+      expect(screen.getByText("Voittajat")).toBeTruthy()
+      expect(screen.getByText("Toiset")).toBeTruthy()
+      
+      expect(screen.getByText("1.")).toBeTruthy()
+      expect(screen.getByText("2.")).toBeTruthy()
+
+      expect(screen.queryByText("Testers")).toBeNull()
+    })
+  })
+})
