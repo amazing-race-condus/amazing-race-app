a5c4781ad3a41ed4cec22aed5ceb24ab973b72ea
kaa <113045348+kaahy@users.noreply.github.com>
2025-06-23 14:53:46 +0300

    Add content in export file

--- frontend/src/components/results/ExportResults.tsx
+++ frontend/src/components/results/ExportResults.tsx
@@ -1,28 +1,138 @@
 import { Button, Platform, View } from "react-native"
 import * as FileSystem from "expo-file-system"
 import * as Sharing from "expo-sharing"
+import { Event, Group } from "@/types"
+import { useSelector } from "react-redux"
+import { RootState } from "@/store/store"
+import { sortByTime } from "@/utils/groupUtils"
+import { getRaceTime } from "@/utils/timeUtils"
 
-const saveResultsFile = async () => {
+const resultRowsText = (event: Event | null, groups: Group[] | null) => {
+  if (!event || !groups)
+    return ""
+
+  let content = ""
+  groups.forEach(group => {
+    const time = getRaceTime(group, event)
+    const timePrint = (time !== null) ? String(Math.round(time/60*100)/100) + " min" : "-"
+    content += statusText(event, group)
+    content += "  "
+    content += progress(group) + " rastia"
+    content += "  "
+    content += timePrint
+    content += "  "
+    content += group.name
+    content += "\n"
+  })
+  return content
+}
+
+const statusText = (event: Event, group: Group, ranking?: number) => {
+  if (group.disqualified)
+    return "Disk."
+  if (group.dnf)
+    return "Kesk."
+  if (!group.route)
+    return "Ei reittiä!"
+  if (group.route.length === 0)
+    return "Ei reittiä!"
+  if (group.route[0].id === group.nextCheckpointId)
+    return "Ei al."
+  if (group.finishTime && !group.disqualified && !group.dnf && group.nextCheckpointId === null)
+    return (event.endTime && ranking) ? "Sija " + ranking : "Sija ?"
+  return "Kesken"
+}
+
+const progress = (group: Group) => {
+  //returns how many checkpoint the group has visited, including start and finish
+
+  if (!group.route)
+    return null
+  if (group.route.length === 0)
+    return null
+  if (group.nextCheckpointId === null && !group.finishTime)
+    return null
+  if (group.nextCheckpointId === null && group.finishTime)
+    return group.route.length
+
+  const routeCheckpointIds = group.route.map(route => route.id)
+
+  return routeCheckpointIds.indexOf(Number(group.nextCheckpointId))
+}
+
+const resultsFileContent = (event: Event, groups: Group[]) => {
+  const normalGroups = groups.filter(group => !group.easy)
+  const easyGroups = groups.filter(group => group.easy)
+
+  let normalPassedGroups = normalGroups.filter(group => group.finishTime && !group.disqualified && !group.dnf && group.nextCheckpointId === null)
+  let easyPassedGroups = easyGroups.filter(group => group.finishTime && !group.disqualified && !group.dnf && group.nextCheckpointId === null)
+
+  normalPassedGroups = sortByTime(normalPassedGroups, event)
+  easyPassedGroups = sortByTime(easyPassedGroups, event)
+
+  const normalOtherGroups = normalGroups.filter(group => (!(group.finishTime && !group.disqualified && !group.dnf && group.nextCheckpointId === null)))
+  const easyOtherGroups = easyGroups.filter(group => (!(group.finishTime && !group.disqualified && !group.dnf && group.nextCheckpointId === null)))
+
+  normalOtherGroups.sort((a, b) => {
+    let progressA = progress(a)
+    let progressB = progress(b)
+    if (progressA === null)
+      progressA = -1
+    if (progressB === null)
+      progressB = -1
+    return progressB - progressA
+  })
+
+  easyOtherGroups.sort((a, b) => {
+    const progressA = progress(a)
+    const progressB = progress(b)
+    if (!progressA || !progressB)
+      return 0
+    return progressA - progressB
+  })
+
+  let content = "Tulokset\n\n"
+
+  content += "Tapahtuman nimi: " + event.name + "\n"
+  content += "Aloitettu: " + event.startTime + "\n"
+  content += "Päätetty: " + event.endTime + "\n\n"
+
+  content += "TAVALLISET RYHMÄT\n\n"
+  content += "Status  Edistys Aika  Ryhmän nimi\n"
+  content += resultRowsText(event, normalPassedGroups)
+  content += resultRowsText(event, normalOtherGroups)
+
+  content += "\nHELPOTETUT RYHMÄT\n\n"
+  content += "Status  Edistys Aika  Ryhmän nimi\n"
+  content += resultRowsText(event, easyPassedGroups)
+  content += resultRowsText(event, easyOtherGroups)
+
+  return content
+}
+
+const saveResultsFile = async (event: Event, groups: Group[]) => {
   const path = FileSystem.cacheDirectory + "tulokset.txt"
-  const content = "Tulokset tulossa"
-  await FileSystem.writeAsStringAsync(path, content, { encoding: FileSystem.EncodingType.UTF8 })
+  await FileSystem.writeAsStringAsync(path, resultsFileContent(event, groups), { encoding: FileSystem.EncodingType.UTF8 })
 }
 
 const shareResultsFile = async () => {
   await Sharing.shareAsync(FileSystem.cacheDirectory + "tulokset.txt")
 }
 
-const exportResults = async () => {
+const exportResults = async (event: Event, groups: Group[]) => {
   const canShare = await Sharing.isAvailableAsync()
   if (canShare && Platform.OS !== "web") {
-    await saveResultsFile()
+    await saveResultsFile(event, groups)
     await shareResultsFile()
   }
 }
 
 const ExportResults = () => {
+  const event = useSelector((state: RootState) => state.event)
+  const groups = useSelector((state: RootState) => state.groups)
+
   return (<View style={{ marginVertical: 10 }}>
-    <Button title="Jaa tulokset" onPress={exportResults} />
+    <Button title="Jaa tulokset" onPress={() => exportResults(event, groups)} />
   </View>)
 }
 
