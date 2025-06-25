aa03d27cd8ff637fd4ea7b21ee12f2e77a4bfdb0
kaa <113045348+kaahy@users.noreply.github.com>
2025-06-25 11:29:34 +0300

    Darken start/end buttons when not expected to be pressed

--- frontend/src/components/settings/GameView.tsx
+++ frontend/src/components/settings/GameView.tsx
@@ -128,11 +128,11 @@ const GameView = () => {
         startedGroups={startedGroups}
         validDistances={validDistances}
       />
-      <Pressable style={({ pressed }) => [styles.bigButton, {opacity: pressed ? 0.5 : 1 }]} onPress={() => handleStart()}>
+      <Pressable style={({ pressed }) => [styles.bigButton, {opacity: pressed || event.startTime ? 0.5 : 1 }]} onPress={() => handleStart()}>
         <Text style={styles.buttonText}>Aloita peli</Text>
       </Pressable>
 
-      <Pressable style={({ pressed }) => [styles.bigButton, {opacity: pressed ? 0.5 : 1 }]} onPress={() => handleEnd()}>
+      <Pressable style={({ pressed }) => [styles.bigButton, {opacity: pressed || event.endTime ? 0.5 : 1 }]} onPress={() => handleEnd()}>
         <Text style={styles.buttonText}>Lopeta peli</Text>
       </Pressable>
       {(event.startTime || event.endTime) &&
