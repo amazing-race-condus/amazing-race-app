466f8685a267737dbae4d1b00cf4cea18f12675c
Eeritvan <144259545+Eeritvan@users.noreply.github.com>
2025-06-26 14:42:51 +0300

    Fetch new routes once new routes are generated

--- frontend/src/websocket/groupSocket.ts
+++ frontend/src/websocket/groupSocket.ts
@@ -1,5 +1,5 @@
 import store from "@/store/store"
-import { updateGroup, removeGroup, appendGroup } from "@/reducers/groupSlice"
+import { updateGroup, removeGroup, appendGroup, fetchGroups } from "@/reducers/groupSlice"
 import { Group } from "@/types"
 
 export const setupGroupHandlers = (socket: any) => {
@@ -17,10 +17,16 @@ export const setupGroupHandlers = (socket: any) => {
   socket.on("group:deleted", (group: Group) => {
     store.dispatch(removeGroup(group))
   })
+
+  socket.on("groups:routes_generated", () => {
+    const currentEventId = store.getState().event.id
+    store.dispatch(fetchGroups(currentEventId))
+  })
 }
 
 export const cleanupGroupHandlers = (socket: any) => {
   socket.off("group:created")
   socket.off("group:updated")
   socket.off("group:deleted")
+  socket.off("groups:routes_generated")
 }
